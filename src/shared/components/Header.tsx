1462f65a6799c43d17b6634997548b223603048e
startoverrox <toplondon08@gmail.com>
2025-10-16 10:38:42 +0900

    feat: use clerk (auth sdk)

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,42 +1,31 @@
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
-import useToastStore from "@/store/toastStore";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
-  const navigate = useNavigate();
-  // const { user } = useUserStore()
-  const { setToast } = useToastStore.getState();
-
   // const darkMode = useSettingStore((state) => state.darkMode);
   // const setDarkMode = useSettingStore((state) => state.setDarkMode);
 
-  const logoutUser = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    // const success = await signout()
-    // if (success) {
-    // 	navigate('/login')
-    // 	setToast({
-    // 		message: 'Logout Success !',
-    // 		type: 'success',
-    // 	})
-    // } else {
-    // 	setToast({
-    // 		message: 'Logout Fail !',
-    // 		type: 'error',
-    // 	})
-    // }
-  };
-
   return (
-    <div className="sticky top-0 z-10 flex items-center justify-between border-b p-4 text-white">
-      <span className="cursor-pointer" onClick={() => navigate("/")}>
+    <header className="sticky top-0 z-10 flex items-center justify-between border-b p-4 text-white">
+      <Link to="/" className="cursor-pointer">
         LOGO
-      </span>
+      </Link>
       {/* <button onClick={() => setDarkMode(darkMode)}>
         {darkMode ? 'dark' : 'light'}
       </button> */}
-      <button onClick={logoutUser}>Logout</button>
-    </div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   );
 };
 
